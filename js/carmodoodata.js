b53cdd9a6a6e7099ce6b14e26e384ebd5aceca73
Joseph Choi <chousemath@gmail.com>
2019-07-15 14:56:17 +0900

    Start adding desktop control script for the carmodoo desktop program

---
+++ js/carmodoodata.js
@@ -0,0 +1,24 @@
+const robot = require("robotjs");
+require('dotenv').config()
+
+// Speed up the mouse.
+robot.setMouseDelay(2);
+
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+(async () => {
+    const screenSize = robot.getScreenSize();
+    const desktopIconLocation = {
+        width: screenSize.width - 50,
+        height: 30,
+    };
+    robot.moveMouse(desktopIconLocation.width, desktopIconLocation.height);
+    robot.mouseClick();
+    robot.keyTap('enter');
+    await sleep(5000);
+    robot.typeString(process.env.CARMODOO_ID);
+    robot.keyTap('tab');
+    robot.typeString(process.env.CARMODOO_PW);
+    robot.keyTap('enter');
+})();
+
