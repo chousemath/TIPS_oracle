0a83c1415c3187f7f3ac02c21d83baba4d216a23
Joseph Choi <chousemath@gmail.com>
2019-07-16 15:50:13 +0900

    Update with .env access

--- py/carmodoo.py
+++ py/carmodoo.py
@@ -1,9 +1,22 @@
 import pyautogui
 import time
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 screen_width, screen_height = pyautogui.size()
 
 time.sleep(5)
 
 pyautogui.click()
+pyautogui.press('enter')
+
+time.sleep(30)
+
+carmodoo_id = os.getenv('CARMODOO_ID')
+carmodoo_pw = os.getenv('CARMODOO_PW')
+
+pyautogui.typewrite(carmodoo_id)
+pyautogui.press('tab')
+pyautogui.typewrite(carmodoo_pw)
 pyautogui.press('enter')
