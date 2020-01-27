81c22a18f9a2de90548783923a1df31429e46180
Joseph Choi <chousemath@gmail.com>
2020-01-27 12:34:30 +0900

    Add more to the decompose script

--- js/decompose.py
+++ js/decompose.py
@@ -5,14 +5,24 @@ from os import listdir, path
 Route = Tuple[str, str]
 
 
-def decompose(src_dir: str, dst_dir: str):
+def decompose(route: Route):
+    src_dir, dst_dir = route
     for fname in (x for x in listdir(src_dir) if '.html' in x):
         with open(path.join(src_dir, fname), 'r') as f:
             contents = f.read()
             soup = BeautifulSoup(contents, 'html.parser')
+            for head in soup.findAll('head'):
+                head.decompose()
+            with open(path.join(dst_dir, fname), 'w') as f:
+                f.write(str(soup))
 
 
 routes: List[Route] = [
     ('pages_detail', 'html_src_encar'),
     ('pages_detail_encar_domestic', 'html_src_encar'),
+    ('pages_detail_mpark', 'html_src_mpark'),
+    ('pages_detail_aj', 'html_src_aj'),
 ]
+
+for route in routes:
+    decompose(route)
