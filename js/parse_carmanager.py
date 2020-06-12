02b06d78d34a4ca2098794aa1f85f4b9f0957a86
Joseph Choi <chousemath@gmail.com>
2020-06-12 09:07:10 +0900

    Update

--- js/parse_carmanager.py
+++ js/parse_carmanager.py
@@ -4,10 +4,12 @@ from os import path, listdir
 import json
 
 root_dirs = ['pages_detail_carmanager']
-for root_dir in root_dirs:
-    for p in (path.join(root_dir, f) for f in listdir(root_dir) if '.html' in f):
-        with open('carmanager.csv','a+') as fd:
-            writer = csv.writer(fd)
+with open('carmanager.csv','a+') as fd:
+    writer = csv.writer(fd)
+    for root_dir in root_dirs:
+        for (p, file_name) in ((path.join(root_dir, f), f) for f in listdir(root_dir) if '.html' in f):
+            # 1580627715862-57841662.html
+            ts = int(file_name.split('-')[0]) // 1000
             try:
                 with open(p, 'r') as f:
                     contents = f.read()
@@ -16,16 +18,16 @@ for root_dir in root_dirs:
                     car_price = int(soup.find(id='ui_ViewCarAmount').text.replace(',', '')) * 10_000
                     car_year = int(soup.find(id='ui_ViewCarRegYear').text)
                     car_regist_year = soup.find(id='ui_ViewCarReg').text
-
+    
                     car_mileage = 0
                     car_transmission = ''
                     car_fuel = ''
                     car_plate_num = ''
-
+    
                     car_options = []
                     for inp in [x for x in soup.find_all('input', checked=True, id=False)]:
                         car_options.append(inp.findNext('label').text.strip())
-
+    
                     for th in soup.find_all('th'):
                         # 미 션, 연 료, 색 상, 차량번호
                         if th.text == '차량번호':
@@ -38,6 +40,7 @@ for root_dir in root_dirs:
                             car_mileage = int(th.findNext('td').text.replace(',', '').replace(' km', '').strip())
                     car_options = '|'.join(car_options)
                     writer.writerow([
+                        ts,
                         car_name,
                         car_price,
                         car_year,
