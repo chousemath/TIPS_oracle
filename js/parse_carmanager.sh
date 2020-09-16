d5989baa013a6486ed1126b6f318c30de8e5a355
Joseph Choi <chousemath@gmail.com>
2020-09-16 16:00:29 +0900

    Update

--- js/parse_carmanager.sh
+++ js/parse_carmanager.sh
@@ -11,4 +11,10 @@ do
     aws s3 cp carmanager_forecast.csv s3://trive-forecast-01/carmanager_forecast.csv
     aws s3 cp carmanager_forecast.json s3://trive-forecast-01/carmanager_forecast.json
     aws s3 cp carmanager_sales.csv s3://trive-forecast-01/carmanager_sales.csv
+    aws s3 cp carmanager_allowed.json s3://trive-forecast-01/carmanager_allowed.json
+    aws s3 cp carmanager_colors.json s3://trive-forecast-01/carmanager_colors.json
+    aws s3 cp carmanager_transmissions.json s3://trive-forecast-01/carmanager_transmissions.json
+    aws s3 cp carmanager_accidents.json s3://trive-forecast-01/carmanager_accidents.json
+    aws s3 cp carmanager_warranties.json s3://trive-forecast-01/carmanager_warranties.json
+    aws s3 cp carmanager_years.json s3://trive-forecast-01/carmanager_years.json
 done
