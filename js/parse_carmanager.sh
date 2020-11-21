619ba087693415c31565ae3e73beafa764e4e8ff
Joseph Choi <chousemath@gmail.com>
2020-11-22 08:44:31 +0900

    Update upload script to s3

--- js/parse_carmanager.sh
+++ js/parse_carmanager.sh
@@ -4,11 +4,10 @@ set -o nounset                              # Treat unset variables as an error
 
 while true
 do
-    python parse_carmanager.py
     python aws_forecast_01.py
     python aws_forecast_02.py
     python carmanager_transmissions.py
-    node cm_selection.js
+    # node cm_selection.js
     aws s3 cp carmanager_forecast.csv s3://trive-forecast-01/carmanager_forecast.csv
     aws s3 cp carmanager_forecast.json s3://trive-forecast-01/carmanager_forecast.json
     aws s3 cp carmanager_sales.csv s3://trive-forecast-01/carmanager_sales.csv
@@ -20,4 +19,5 @@ do
     aws s3 cp carmanager_years.json s3://trive-forecast-01/carmanager_years.json
     aws s3 cp cm_selection.json s3://trive-forecast-01/cm_selection.json
     aws s3 cp allowed_by_color.json s3://trive-forecast-01/allowed_by_color.json
+    sleep 5h
 done
--- js/upload_to_s3.sh
+++ js/upload_to_s3.sh
@@ -6,6 +6,7 @@ while true
 do
     # python parse_encar_new.py
     aws s3 cp encar_new2.csv s3://trive-forecast-01/encar_new2.csv
-    echo 'sleeping for 10h'
-    sleep 10h
+    aws s3 cp carmanager.csv s3://trive-forecast-01/carmanager.csv
+    echo 'sleeping for 5h'
+    sleep 5h
 done
